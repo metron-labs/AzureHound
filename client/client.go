ff456f4a60c1d83331d19a6794aee612523c7194
vishalk-metron <vishal.khemnar@metronlabs.com>
2025-07-09 11:50:53 +0530

    Code Cleaning
    Removed Unused Methods. Commented Code

--- client/client.go
+++ client/client.go
@@ -259,12 +259,8 @@ type AzureClient interface {
 	CloseIdleConnections()
 
 	// New methods for role detection
-	// In client/client.go
 	GetUserDirectoryRoles(ctx context.Context, userPrincipalName string) ([]azure.DirectoryRole, error)
 	GetUserMemberOf(ctx context.Context, userPrincipalName string) ([]azure.DirectoryObject, error)
-
-	// GetUserDirectoryRoles(ctx context.Context, userPrincipalName string) ([]DirectoryRole, error)
-	// GetServicePrincipal(ctx context.Context, userPrincipalName string) (*ServicePrincipal, error)
 
 	CollectSessionDataDirectly(ctx context.Context) <-chan AzureResult[azure.DeviceSessionData]
 	GetUserSignInActivity(ctx context.Context, userPrincipalName string, days int) ([]SignInEvent, error)
